bc75a0c4b08f2f662923102c1eab74e363b758e6
BlackJet <brox2319@gmail.com>
2014-05-15 22:49:07 +0400

    ~

--- src/contest/talentbuddy/projects/SearchEngine.java
+++ src/contest/talentbuddy/projects/SearchEngine.java
@@ -8,6 +8,8 @@ public class SearchEngine {
 
     public static void main(String[] args) {
 
+        tokenize_query("car? dealers! bmw, audi", "?!");
+
     }
 
     public static void remove_stopwords(String[] query, String[] stopwords) {
@@ -23,6 +25,26 @@ public class SearchEngine {
                 System.out.println(word);
             }
         }
+    }
+
+    public static void tokenize_query(String query, String punctuation) {
+
+        StringBuilder token = new StringBuilder();
+        for (int i = 0; i < query.length(); i++) {
+            char c = query.charAt(i);
+            boolean separator = punctuation.indexOf(c) != -1 || c == ' ';
+            if (separator) {
+                if (token.length() > 0) {
+                    System.out.println(token);
+                    token.setLength(0);
+                }
+                continue;
+            }
+            token.append(c);
+        }
+
+        System.out.println(token);
+
     }
 
 }
